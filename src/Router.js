c803fb4a24f8e6636c00c81207c3aebb41378996
hacker092322 <hacker092322@hotmail.com>
2022-02-19 11:01:46 +0700

    Update chat design.

--- src/Router.js
+++ src/Router.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
 import Profile from './Profile.js';
+import Userlist from "./Userlist";
 
 function Router(){
 return (
@@ -8,6 +9,7 @@ return (
      <Routes>
        <Route path="/" element={ <App/>} />
        <Route path="/Profile" element={ <Profile/> } />
+       <Route path="/Userlist" element={ <Userlist/> } />
      </Routes>
     </BrowserRouter>
  )
